124b6ad0763789414694662f8eee79a519a933fa
jcrops <suttar@gmail.com>
2021-03-04 16:05:56 -0500

    Got occupied spaces working

--- script.js
+++ script.js
@@ -6,9 +6,47 @@ const gameBoard =(() => {
 
     const players = []
 
+    const checkWin = (anArray) => {
+        
+        let winState =[
+
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+
+            [0, 4, 8],
+            [6, 4, 2]
+        ]
+        
+        let indicies = []
+
+        anArray.forEach((element, index) => {
+            if(element !== ""){
+                indicies.push(index);    
+            }
+        });
+
+        for (i=0; i<winState.length; i++){
+            if (gameBoard.board[i] === winState[i]){
+                console.log('winner')
+            }
+        }
+
+        return indicies
+
+    
+
+    }
+    
+
     return {
         board,
-        players
+        players,
+        checkWin
     };
 
 })();
@@ -29,10 +67,16 @@ const gameFlow = (p1, p2) =>{
     
     let currentPlayer = p1;    
    
-    
+    displayGrid.addEventListener('mouseover', (e) =>{
+
+        if (e.target.className === 'cell')
+        {e.target.classList.add(`${currentPlayer.marker}hover`)}
+         
+    });
+
     displayGrid.addEventListener('click', (e) => {
 
-        if (e.target.className === 'cell') {
+        if (e.target.className.charAt(0,1) === 'c') {
         
             if (gameBoard.board[e.target.id.charAt(1)] === ""){
 
@@ -44,8 +88,20 @@ const gameFlow = (p1, p2) =>{
             
             DisplayController.displayGrid(gameBoard.board);
 
+
+            let check = gameBoard.checkWin(gameBoard.board)
+
+            console.log("occupied spaces are:", check)
+            console.log("the board contains:", gameBoard.board)
+
             }
+
+    
     });
+
+
+
+
 };
 
 const DisplayController = (() => {
@@ -74,6 +130,8 @@ const DisplayController = (() => {
 
     return {displayGrid, displayPlayer};
 })();
+
+
 
 const playGame = () => { 
     let player1 = createPlayer("X")
